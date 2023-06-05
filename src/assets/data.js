995d3d5b316667330945b7f373f9d0d5fe9bbce7
Lucas Baldin <lucasbaldin1311@gmail.com>
2023-06-05 11:22:34 -0300

    imp/Updated About description

--- src/assets/data.js
+++ src/assets/data.js
@@ -14,11 +14,12 @@ const data = {
     about: {
         title: "My Background",
         description:
-            `I worked for about 3 years in a Junior Company, as a Web developer and Chief Legal Officer
-            Started working in Ci&T in 2018, as an intern, on a Support and Legacy Optimization team, answering L2 tickets and developing L3 tickets, through AEM.
-            I have 2 years of experience being an automation QA, developing tests for Lambda functions, and also creating an automation tool in NodeJS.
-            Already developed web services and integrations for Chat Bots, using Azure and Microsoft Framework and experienced the creation of APIs using Lambda function made in NodeJS. 
-            Currently working with Scrum, managing rites, collecting metrics and facilitating team processes.`,
+            `Hello, my name is Lucas Baldin, and  I'd like to share a summary of my career so far =)\n
+            I began my professional journey as a Web Developer at Info Jr., a junior company, where I gained experience in web development. I then took on the role of Executive/Financial Director at the same junior company, honing my management and leadership skills.\n
+            Subsequently, I joined CI&T as an intern, working in a Support and Optimization team. During this time, I developed problem-solving skills and worked with the Adobe Experience Manager (AEM) platform also using the ITIL best practices as reference.\n
+            As I progressed at CI&T, I was promoted to Software Engineer, where I had the opportunity to specialize in creating chatbots using technologies like BlipChat and Microsoft Bot. Additionally, I gained expertise in Node.js, Azure Functions, Cosmos DB, and Power BI, among other relevant technologies for building efficient solutions.\n
+            More recently, I held the position of Senior Software Engineer at CI&T, where I was responsible for developing APIs using Node.js Lambda Functions. I also worked on integrations with databases such as Dynamo, Redis, and Aurora, as well as utilizing GraphQL and Location Services. Furthermore, I had the chance to develop the frontend using VUE 3 and expose it through AEM.\n
+            Currently, I am working as a Scrum Master at CI&T, leading the agile process, collecting metrics, facilitating Scrum rituals, and collaborating on project planning and delivery. I am committed to fostering teamwork and maintaining close relationships with clients.`,
     },
     skills: [
         {
--- src/partials/About.js
+++ src/partials/About.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 const About = ({ title, description }) => {
+    const lines = description.split("\n");
+
     return (
         <div className="max-w-4xl mx-auto mt-16">
             <p className="text-2xl md:text-4xl font-bold text-center text-gray-300">
                 {title}
             </p>
-            <p className="text-base text-justify md:text-center text-gray-500 leading-relaxed mt-4">
-                {description}
-            </p>
+            {lines.map((line, index) => {
+                if (line.trim() === "") return null;
+
+                return (
+                    <React.Fragment key={index}>
+                        <p className="text-base text-justify text-gray-500 leading-relaxed mt-4">
+                            {line}
+                        </p>
+                        {index !== lines.length - 1 && <br/>}
+                    </React.Fragment>
+                );
+            })}
         </div>
     );
 };
